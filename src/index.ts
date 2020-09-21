e9ca8aa0d7b6727e03b19647a2df8c18d3c28353
Kim taejun <joon5006@naver.com>
2020-09-21 18:22:49 +0900

    Setting Project(typescript, graphql, prisma)

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { Options } from "graphql-yoga";
+import app from "./app";
+dotenv.config();
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GRAPHQL_ENDPOINT,
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+
+app.start(appOptions, handleAppStart);
